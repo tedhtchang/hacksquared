cdb530af1ac4da926df34b7b21648ceb238d386f
Chris Hupman <chris.hupman@gmail.com>
2017-03-06 13:01:21 -0800

    Added placeholder for motd generator

--- app.go
+++ app.go
@@ -23,12 +23,20 @@ var converter = template.Must(template.ParseFiles(
   "templates/us2sane.html",
 ))
 
+var messagegen = template.Must(template.ParseFiles(
+  "templates/_base.html",
+  "templates/motd.html",
+))
 func helloworld(w http.ResponseWriter, req *http.Request) {
   index.Execute(w, nil)
 }
 
 func us2sane(w http.ResponseWriter, req *http.Request) {
   converter.Execute(w, nil)
+}
+
+func motd(w http.ResponseWriter, req *http.Request) {
+  messagegen.Execute(w, nil)
 }
 
 func main() {
@@ -39,6 +47,7 @@ func main() {
 
 	http.HandleFunc("/", helloworld)
 	http.HandleFunc("/converter", us2sane)
+	http.HandleFunc("/motd", motd)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Printf("Starting app on port %+v\n", port)
